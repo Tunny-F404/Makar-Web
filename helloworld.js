147e0bbfec44f7cb9c9b078b4a8b7bca3be3a09a
Makar <wuyunyuf404@gmail.com>
2024-10-09 19:32:37 +0800

    modify hello```

--- helloworld.js
+++
@@ -1,8 +0,0 @@
-console.log('HelloWorld');
-console.log('add onononono');//24.10.9 add
-console.log('modify helloworld');//24.10.9.18.47
-
-console.log('add new helloworld');//24.10.9.19.14
-console.log('again modify helloworld');
-
-console.log('modify add new helloworld');
